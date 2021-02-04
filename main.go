e85cb252430f06465f1159abf16a0516547a2c25
marcus <5216011i@gmail.com>
2021-02-04 23:53:37 +0900

    sqlからcsvに変換 次回はcsv加工できるように

--- main.go
+++ main.go
@@ -2,25 +2,61 @@ package main
 
 import (
 	"database/sql"
+	"encoding/csv"
+	"flag"
 	"fmt"
 	"log"
+	"os"
+
+	"github.com/joho/sqltocsv"
 
 	_ "modernc.org/sqlite"
 )
 
 func main() {
-	db, err := sql.Open("sqlite", "./test.sqlite3")
+	var sqlField string
+
+	// accept file name from CLI
+	flag.StringVar(&sqlField, "file name", "", `Put file youd like to convert to csvfile`)
+	flag.Parse()
+
+	// open database
+	db, err := sql.Open("sqlite", "./"+sqlField)
 	if err != nil {
 		log.Fatal(err)
 	}
+	// close data base
 	defer db.Close()
 
-	result, err := db.Exec(`SELECT * FROM test`)
+	// select all data from a table
+	result, err := db.Query(`Select * from test`)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer result.Close()
-	fmt.Printf("result: %s", result)
+	// reflect response from database to csv
+	csvConverter := sqltocsv.New(result)
+	// make csv file
+	csvConverter.WriteFile("result.csv")
+	//  read csv file
+	// open csv file
+	file, err := os.Open("./result.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// read opened file as a csvfile
+	reader := csv.NewReader(file)
+
+	var line []string
+
+	for {
+		line, err = reader.Read()
+		if err != nil {
+			break
+		}
+		fmt.Printf("%v", line)
+	}
 
 	err = db.Ping()
 	if err != nil {
